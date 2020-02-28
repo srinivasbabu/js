476fb9f694267509857354dccc3dccf1a46e90de
srinivasbabu <gr.srinivasbabu@gmail.com>
2020-02-28 18:21:03 +0530

    Added Object function

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ let textObj = {
 }
 
 console.log(textObj.myFunc());
+
+const rect = {
+    length: 5,
+    height: 7,
+    location: {
+        x: 11,
+        y:12
+    },
+    area: function(){
+        return this.length * this.height
+    }
+}
+
+const rect1 = {
+    length: 5,
+    height: 6,
+    location: {
+        x: 11,
+        y:12
+    },
+    area: function(){
+        return this.length * this.height
+    }
+}
+
+console.log(rect.area());
